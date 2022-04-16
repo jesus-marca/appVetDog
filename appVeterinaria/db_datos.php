0c947feb9be6780482e6198fb408004b274c6ae3
jesus-marca <44207881+jesus-marca@users.noreply.github.com>
2022-04-16 00:53:41 -0500

    Update db_datos.php

--- appVeterinaria/db_datos.php
+++ appVeterinaria/db_datos.php
@@ -1,9 +1,13 @@
 <?php
     //$conn = mysqli_connect("localhost", "root","", "app");
     //$conn = mysqli_connect("jtb9ia3h1pgevwb1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "uq2hk1qexkl7sepo","v4zzjfa34j0yra1f", "mmxkis2yga6v7kej");
-    $db_host="localhost";
+    /*$db_host="localhost";
     $db_user="root";
     $db_pass="";
-    $db_database="veterinaria";
+    $db_database="veterinaria";*/
+    $db_host="jtb9ia3h1pgevwb1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $db_user="uq2hk1qexkl7sepo";
+    $db_pass="epvcime5dlhknb7g";
+    $db_database="mmxkis2yga6v7kej";
 
 ?>
